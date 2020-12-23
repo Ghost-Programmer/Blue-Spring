ec7e4cab2f3b315f5119602e31758ab8eb3ef7f2
Jeff Miller <jmiller@tjctechnology.com>
2020-12-22 21:02:00 -0500

    Swagger Login working

--- src/main/java/com/nrha/reinersuite/config/SpringFoxConfig.java
+++ src/main/java/com/nrha/reinersuite/config/SpringFoxConfig.java
@@ -1,11 +1,24 @@
 package com.nrha.reinersuite.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;;
 
 @Configuration
 public class SpringFoxConfig {
@@ -15,6 +28,31 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Arrays.asList(securityScheme()));
+    }
+
+    private SecurityScheme securityScheme() {
+        List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
+
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("/oauth/token");
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(authorizationScopeList)
+                .build();
+        return oauth;
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId("appClient")
+                .clientSecret("password")
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
                 .build();
     }
 }
