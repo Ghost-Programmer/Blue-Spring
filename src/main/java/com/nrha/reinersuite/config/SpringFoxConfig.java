20b29c219f0f014a4e9559376163bc37ece66255
Jeff Miller <jmiller@tjctechnology.com>
2020-12-25 16:50:18 -0500

    Working as Entering Token

--- src/main/java/com/nrha/reinersuite/config/SpringFoxConfig.java
+++ src/main/java/com/nrha/reinersuite/config/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package com.nrha.reinersuite.config;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -22,37 +23,41 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;;
 
 @Configuration
 public class SpringFoxConfig {
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(securityScheme()));
-    }
-
-    private SecurityScheme securityScheme() {
-        List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
-        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
-
-        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("/oauth/token");
-
-        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
-                .grantTypes(Arrays.asList(grantType))
-                .scopes(authorizationScopeList)
                 .build();
-        return oauth;
     }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId("appClient")
-                .clientSecret("password")
-                .scopeSeparator(" ")
-                .useBasicAuthenticationWithAccessCodeGrant(true)
-                .build();
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "1.0",
+                "Terms of service",
+                new Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
     }
 }
