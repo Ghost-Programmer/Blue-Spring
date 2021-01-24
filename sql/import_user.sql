561658443be1a12bc00482cf32486a33e380c100
Jeff Miller <jmiller@tjctechnology.com>
2021-01-24 08:35:22 -0500

    Coreccted SQL

--- sql/import_user.sql
+++ sql/import_user.sql
@@ -1,8 +1,8 @@
 insert into nrha_user.user (id, username,password,enabled,account_expired,password_change_required,account_locked,account_verified,account_complete,account_refreshed,date_created,last_updated) select id, username,password,enabled,account_expired,password_change_required,account_locked,account_verified,account_complete,account_refreshed,date_created,last_updated from core.user;
 
 # TODO: Change to correct roles
-insert into nrha_user.user_security_role (user_id,security_role_id,date_created,last_updated) select id,1,now(),now() from nrha.user;
-insert into nrha_user.user_security_role (user_id,security_role_id,date_created,last_updated) select id,4,now(),now() from nrha.user;
+insert into nrha_user.user_security_role (user_id,security_role_id,date_created,last_updated) select id,1,now(),now() from core.user;
+insert into nrha_user.user_security_role (user_id,security_role_id,date_created,last_updated) select id,4,now(),now() from core.user;
 update nrha_user.user set account_expired = 0;
 update nrha_user.user set account_locked = 0;
 
--- sql/nrha_user.sql
+++ sql/nrha_user.sql
@@ -143,7 +143,7 @@ INSERT INTO `nrha_user`.`oauth_client_details` (`client_id`, `resource_ids`, `cl
 VALUES ('appClient', 'nrha', '$2a$10$Ob7Xv7QF3MnrTyIgV6J7Qu5T89zxaHJlT0i7aYW876hizquvUYg.m', 'read,write', 'password,authorization_code,check_token,refresh_token,client_credentials', 'http://localhost:8080', 'ROLE_CLIENT', '1800', '172800', 1);
 
 INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_USER', 'User', 'User', 'Grants Access to Login', 1, now());
-INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_USER_MANAGEMENT', 'User Management', 'Admin', 'Modify User Role Access.', 0, now());
+INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_USER_MANAGEMENT', 'User Management', 'Admin', 'Modify User Role Access.', 0, now());
 INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_CHANGE_USER_PASSWORD', 'Change User Password', 'Admin', 'Permission to change a password for a user.', 0, now());
 INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_CHANGE_OWN_PASSWORD', 'Change Own Password', 'User', 'Permission to change own password.', 1, now());
 INSERT INTO `nrha_user`.`security_role` (`authority`, `name`, `category`, `description`, `default`, `date_created`) VALUES ('ROLE_DEVELOPER', 'Developer', 'Dev', 'Developer special access.', 0, now());
