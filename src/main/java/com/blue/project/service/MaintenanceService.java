ab9cab4cc532ba3124e88ee6810408d20ff4eb73
Jeff Miller <jmiller@tjctechnology.com>
2021-03-16 09:12:59 -0400

    Maintenance Window

--- src/main/java/com/blue/project/service/MaintenanceService.java
+++ src/main/java/com/blue/project/service/MaintenanceService.java
@@ -1,10 +1,13 @@
 package com.blue.project.service;
 
 import com.blue.project.dto.ScheduledMaintenance;
+import com.blue.project.dto.mainteance.MaintenanceSearch;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface MaintenanceService {
 
     @PreAuthorize("permitAll()")
     ScheduledMaintenance getNextScheduledMaintenance();
+
+    MaintenanceSearch search(MaintenanceSearch search);
 }
--- src/main/java/com/blue/project/service/MaintenanceServiceImpl.java
+++ src/main/java/com/blue/project/service/MaintenanceServiceImpl.java
@@ -2,8 +2,12 @@ package com.blue.project.service;
 
 import com.blue.project.dao.maintenance.ScheduleRepository;
 import com.blue.project.dto.ScheduledMaintenance;
+import com.blue.project.dto.mainteance.MaintenanceSearch;
 import com.blue.project.models.maintenance.Scheduled;
+import com.blue.project.models.users.User;
+import name.mymiller.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,5 +28,27 @@ public class MaintenanceServiceImpl implements MaintenanceService {
         if (maintenance == null) {
             return null;
         } else return new ScheduledMaintenance(maintenance);
+    }
+
+    public MaintenanceSearch search(MaintenanceSearch search) {
+        Pageable pageable;
+        Page<Scheduled> results;
+
+        if (StringUtils.isNotNullOrEmpty(search.getSort())) {
+            if (search.getAscending()) {
+                pageable = PageRequest.of(search.getPage(), search.getSize(), Sort.by(search.getSort()).ascending());
+            } else {
+                pageable = PageRequest.of(search.getPage(), search.getSize(), Sort.by(search.getSort()).descending());
+            }
+        } else {
+            pageable = PageRequest.of(search.getPage(), search.getSize());
+        }
+
+        results = this.scheduleRepository.findAll(pageable);
+
+        search.setResults(results.toList());
+        search.setTotal(results.getTotalElements());
+
+        return search;
     }
 }
