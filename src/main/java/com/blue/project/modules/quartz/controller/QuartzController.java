03bf773e9608175eebbaf1c95e8e5413235f9c37
Jeff Miller <jmiller@tjctechnology.com>
2021-04-23 22:25:36 -0400

    Pause/Resume/Delete Job controls added

--- src/main/java/com/blue/project/modules/quartz/controller/QuartzController.java
+++ src/main/java/com/blue/project/modules/quartz/controller/QuartzController.java
@@ -1,15 +1,13 @@
 package com.blue.project.modules.quartz.controller;
 
+import com.blue.project.dto.StatusMessage;
 import com.blue.project.modules.quartz.dto.QuartzJobInfo;
 import com.blue.project.modules.quartz.services.QuartzService;
 import com.blue.project.modules.users.dto.UserRole;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
@@ -31,5 +29,35 @@ public class QuartzController {
     @GetMapping("/groups")
     public List<String> getGroupNames() throws SchedulerException {
         return this.quartzService.getGroupNames();
+    }
+
+    @RolesAllowed("ROLE_ADMIN_QUARTZ")
+    @PostMapping("/group/{group}/job/{jobName}/pause")
+    public StatusMessage pauseJob(@PathVariable("jobName") String jobName, @PathVariable("group") String group) throws SchedulerException {
+        this.quartzService.pauseJob(jobName,group);
+
+        return new StatusMessage().setOk(true);
+    }
+
+    @RolesAllowed("ROLE_ADMIN_QUARTZ")
+    @PostMapping("/group/{group}/job/{jobName}/resume")
+    public StatusMessage resumeJob(@PathVariable("jobName") String jobName, @PathVariable("group") String group) throws SchedulerException {
+        this.quartzService.resumeJob(jobName,group);
+
+        return new StatusMessage().setOk(true);
+    }
+
+    @RolesAllowed("ROLE_ADMIN_QUARTZ")
+    @PostMapping("/group/{group}/job/{jobName}")
+    public StatusMessage executeJob(@PathVariable("jobName") String jobName, @PathVariable("group") String group) throws SchedulerException {
+        this.quartzService.triggerJob(jobName,group);
+
+        return new StatusMessage().setOk(true);
+    }
+
+    @RolesAllowed("ROLE_ADMIN_QUARTZ")
+    @DeleteMapping("/group/{group}/job/{jobName}")
+    public StatusMessage deleteJob(@PathVariable("jobName") String jobName, @PathVariable("group") String group) throws SchedulerException {
+        return new StatusMessage().setOk(this.quartzService.deleteJob(jobName,group));
     }
 }
