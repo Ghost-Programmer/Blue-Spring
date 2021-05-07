62651eeebc873a75f99bc80b0034233ecb8a2e93
Jeff Miller <jmiller@tjctechnology.com>
2021-05-06 20:21:05 -0400

    Calendar Public

--- src/main/java/com/blue/project/modules/calendar/controller/PublicCalendarController.java
+++ src/main/java/com/blue/project/modules/calendar/controller/PublicCalendarController.java
@@ -6,22 +6,24 @@ import com.blue.project.modules.calendar.services.CalendarService;
 import com.blue.project.modules.calendar.dto.EventData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/calendar", produces = MediaType.APPLICATION_JSON_VALUE)
-public class CalendarController {
+@RequestMapping(value = "/public/calendar", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PublicCalendarController {
 
     @Autowired
     private CalendarService calendarService;
 
-    @RolesAllowed("ROLE_USER")
+    @PreAuthorize("permitAll()")
     @PostMapping("/events")
     public List<EventData> getCalendarEventData(@RequestBody EventRange range) {
         return this.calendarService.getEventData(range.getStart(),range.getEnd());
